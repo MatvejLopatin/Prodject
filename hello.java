495422fa9fb2abd17b1529ba011d12a6016049b2
MatvejLopatin <matvejlopatin2009@gmail.com>
2024-01-23 21:41:23 +0400

    dddd

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
 public class hello {
-    public static void main(String[] args) {
-        info("dddd");
-    }
     
     public static void info(String word) {
         System.out.println(word);
+    }
+    public static void main(String[] args) {
+        info("dddd");
     }
 }
