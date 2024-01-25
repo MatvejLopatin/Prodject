a6d7c32cce050eed591f9dd542901ef2080f12b8
MatvejLopatin <matvejlopatin2009@gmail.com>
2024-01-25 17:00:51 +0400

    xz

---
+++ xz.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class xz {
+    public static void main(String[] args) {
+        int summ = 1;
+        for (int i = 1; i <= 5; i++){
+            summ *= i;
+        }   
+        System.out.println(summ);
+    }   
+
+}
