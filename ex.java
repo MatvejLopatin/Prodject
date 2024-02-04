ef61aab29f653638cabdff13176cc84dde59994b
MatvejLopatin <matvejlopatin2009@gmail.com>
2024-02-04 14:26:51 +0400

    new

--- ex.java
+++ ex.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class ex {
     public static void main(String[] args) {
 
---
+++ game.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class game {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberToGuess = (int) (Math.random() * 100); // Генерируем случайное число от 0 до 100
+        int guess;
+        
+        System.out.println("Добро пожаловать в игру 'Угадай число'!");
+        
+        do {
+            System.out.print("Введите ваше предположение: ");
+            guess = scanner.nextInt();
+            
+            if (guess < numberToGuess) {
+                System.out.println("Загаданное число больше.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Загаданное число меньше.");
+            }
+        } while (guess != numberToGuess);
+        
+        System.out.println("Поздравляем! Вы угадали число " + numberToGuess + "!");
+    }
+}
