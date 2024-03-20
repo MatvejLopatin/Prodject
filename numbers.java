2dfb6f5f9d90d70c8b95924d80ad036d21f81569
MatvejLopatin <matvejlopatin2009@gmail.com>
2024-03-20 08:58:53 +0400

    first

---
+++ numbers.java
@@ -0,0 +1,20 @@
+public class numbers {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 8, 1, 3};
+        int d = 0;
+
+        for (int i = arr.length-1; i >= 0; i--) {
+            for (int j = arr.length-1; j >= 0; j--) {
+                if (arr[i] > arr[j]) {
+                    d = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = d;
+                }
+            }
+        }
+    
+        for (int s : arr) {
+            System.out.println(s);
+        }
+    }
+}
