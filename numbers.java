c6383ed06568bb1dfe7bb24c35a3bd07fcceb026
MatvejLopatin <matvejlopatin2009@gmail.com>
2024-03-21 14:19:07 +0400

    d

--- numbers.java
+++ numbers.java
@@ -1,12 +1,11 @@
 public class numbers {
     public static void main(String[] args) {
         int[] arr = {2, 4, 8, 1, 3, 5, 0, 7, 6, -1};
-        int d = 0;
 
         for (int i = arr.length-1; i >= 0; i--) {
             for (int j = arr.length-1; j >= 0; j--) {
                 if (arr[i] > arr[j]) {
-                    d = arr[j];
+                    int d = arr[j];
                     arr[j] = arr[i];
                     arr[i] = d;
                 }
