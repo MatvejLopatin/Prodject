c6ff6c510313e24522148b27d708fc77b5cac937
MatvejLopatin <matvejlopatin2009@gmail.com>
2024-03-22 13:33:27 +0400

    подправка

--- example.java
+++ example.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class example {
     public static void main(String[] args) throws IOException {
         try (FileWriter file = new FileWriter("txt.txt", true)) {
-            Scanner scanner = new Scanner(System.in);
-            String a = scanner.nextLine();
-            file.append(String.valueOf(a));
+            try (Scanner scanner = new Scanner(System.in)) {
+                String a = scanner.nextLine();
+                file.append(String.valueOf(a));
+            }
             file.flush();
         }
     }
