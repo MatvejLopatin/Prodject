3fcb7b15378637a5fbd43ebe0fb59d39f3039175
MatvejLopatin <157027407+MatvejLopatin@users.noreply.github.com>
2024-06-05 20:09:22 +0400

    first commit56

---
+++ src/www.java
@@ -0,0 +1,5 @@
+public class www {
+    public static void main(String[] args) {
+        System.out.println("https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.youtube.com/watch%3Fv%3Dj-iheFkstFQ&ved=2ahUKEwiqw-Te3vWFAxVvKBAIHUvRDq4QwqsBegQIChAF&usg=AOvVaw0LnbZkEht5ZlmSZSNjBbJI".getBytes());
+    }
+}
